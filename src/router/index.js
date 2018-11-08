97884ecf192b44a0c220fb354031c0eaf9d8fd9d
Joy <827270377@qq.com>
2018-11-08 15:39:31 +0800

    new file

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,79 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import PagesView from '@/view/PagesView'
+import HomeView from '@/views/HomeView'
+import MovieView from '@/views/MovieView'
+import BookView from '@/views/BookView'
+import StatusView from '@/views/StatusView'
+import GroupView from '@/views/GroupView'
+import SubjectView from '@/views/SubjectView'
+import DetailView from '@/views/DetailView'
+import SearchView from '@/views/SearchView'
+import LoginView from '@/views/LoginView'
+import RegisterView from '@/views/RegisterView'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/pages/'
+    },
+    {
+      path: '/pges',
+      component: PagesView,
+      children: [{
+        path: '',
+        redirect: '/pages/home'
+      }, {
+        path: 'home',
+        name: 'HomeView',
+        component: HomeView
+      }, {
+        path: 'movie',
+        name: 'MovieView',
+        component: MovieView
+      }, {
+        path: 'book',
+        name: 'BookView',
+        component: BookView
+      }, {
+        path: 'status',
+        name: 'StatusView',
+        component: StatusView
+      }, {
+        path: 'group',
+        name: 'GroupView',
+        component: GroupView
+      }, {
+        path: 'detail/:id',
+        name: 'DetailView',
+        component: DetailView
+      }]
+    }, {
+      path: 'pages/:classify/subject/:id',
+      name: 'SubjectView',
+      component: {
+        default: PagesView,
+        subject: SubjectView
+      }
+    }, {
+      path: '/search',
+      name: 'SearchView',
+      components: {
+        default: PagesView,
+        search: SearchView
+      }
+    }, {
+      path: '/login',
+      name: 'LoginView',
+      component: LoginView
+    }, {
+      path: '/register',
+      name: 'RegisterView',
+      component: RegisterView
+    }, {
+      path: '*',
+      redirect: '/pages/'
     }
   ]
 })
---
+++ src/views/PagesView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'pages-view'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
+
+
